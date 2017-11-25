fa47da1533c82d9fee5e6f308837168f1ef8108d
thomas.caycedo <thomascaycedo19@outlook.com>
2017-11-24 21:30:39 -0500

    Se arreglan errores

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ El objeto con las propiedades del formulario
 
 var p = {
 
-    entradas: document.querySelector("input.validar"),
-    valor: null
+	entradas: document.querySelectorAll("input.validar"),
+	valor: null
 
 }
 
@@ -15,23 +15,33 @@ El objeto con los metodos del formulario
 */
 
 var m = {
-    inicioFormulario: function () {
 
-        for (var i = 0; i < p.entradas.length; i++) {
-            p.entradas[i].addEventListener("focus",m.enFoco)
-        }
-    },
-    enFoco: function(input){
-        p.valor = input.target.value;
-        
-        if (p.valor == ""){
-            
-            document.querySelector("#"input.target.id)= "rgba(255,255,0,0.5)"
-            
-            document.querySelector("[for="+input.target.id+"].obligatorio").style.opacity = 1;
-            
-        }
-    }
+	inicioFormulario: function(){
+
+		for(var i = 0; i < p.entradas.length; i++){
+
+			p.entradas[i].addEventListener("focus", m.enFoco)
+
+		}
+
+	},
+
+	enFoco: function(input){
+		
+		p.valor = input.target.value;
+		
+		if(p.valor == ""){
+
+			document.querySelector("#"+input.target.id).style.background = "rgba(255,255,0,.5)";
+
+			document.querySelector("[for="+input.target.id+"] .obligatorio").style.opacity = 1;
+
+		}
+
+	}
+
 }
 
 m.inicioFormulario();
+
+
